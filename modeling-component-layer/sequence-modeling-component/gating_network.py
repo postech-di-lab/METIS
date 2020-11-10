384d21ff8b3d6cb6c2a95d8bb87738ebb6740d8a
포항공과대학교 황준영 <jyhwang@postech.ac.kr>
2020-11-10 17:58:09 +0900

    Feat: implement prediction layer

--- modeling-component-layer/sequence-modeling-component/gating_network.py
+++ modeling-component-layer/sequence-modeling-component/gating_network.py
@@ -12,7 +12,6 @@ class HGN(nn.Module):
         L = args.sequence_length
         dims = args.dims
 
-        # *validation, padding_idx =0.        
         self.U = nn.Embedding(num_users, dims)
         self.E = nn.Embedding(num_items, dims, padding_idx=0)
 
@@ -22,7 +21,6 @@ class HGN(nn.Module):
         self.instance_gate_item = Variable(torch.zeros(dims, 1).type(torch.FloatTensor), requires_grad=True).cuda()
         self.instance_gate_user = Variable(torch.zeros(dims, L).type(torch.FloatTensor), requires_grad=True).cuda()
         
-        # *validation
         self.instance_gate_item = torch.nn.init.xavier_uniform_(self.instance_gate_item)
         self.instance_gate_user = torch.nn.init.xavier_uniform_(self.instance_gate_user)
 
@@ -48,3 +46,48 @@ class HGN(nn.Module):
         instg_S_i = instg_S_i/ torch.sum(g_score, dim=1).unsqueeze(1)
 
         return instg_S_i
+
+    def prediction_layer(self, S_i,u_i, instg_S_i, q_j,qb_j, for_pred=False):
+        
+        if for_pred:
+            q_j = q_j.squeeze()
+            qb_j = qb_j.squeeze()
+            
+            # long-term
+            res = u_i.mm(q_j.t()) + qb_j
+            
+            # short-term
+            res += instg_S_i.mm(q_j.t())
+            
+            # item-item
+            rel_score = torch.matmul(S_i, q_j.t().unsqueeze(0))
+            rel_score = torch.sum(rel_score, dim=1)
+            res += rel_score
+        else:
+            # long-term
+            res = torch.baddbmm(qb_j, q_j, u_i.unsqueeze(2)).squeeze()
+
+            # short-term
+            res += torch.bmm(instg_S_i.unsqueeze(1), q_j.permute(0, 2, 1)).squeeze()
+            
+            # item-item
+            rel_score = S_i.bmm(q_j.permute(0, 2, 1))
+            rel_score = torch.sum(rel_score, dim=1)
+            res += rel_score
+            
+        return res
+       
+    def forward(self, item_seq_indices, user_indices, items_to_predict, for_pred=False):
+       
+        # embedding look-up 
+        S_i,u_i,q_j,qb_j = self.lookup_layer(item_seq_indices, user_indices, items_to_predict)
+
+        # feature gating
+        featg_S_i = self.feature_gating_layer(S_i,u_i)
+
+        # instance gating
+        instg_S_i = self.instance_gating_layer(featg_S_i,u_i)
+        
+        #return preference scores
+        return self.prediction_layer(S_i,u_i,instg_S_i,q_j,qb_j,for_pred)
+
